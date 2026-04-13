304f7442673fbe687ebb77092236fb9fbfc90f94
Spratch <contact@josephclenet.fr>
2026-04-13 19:38:54 +0200

    build: init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="bg-blue-300 inset-0 absolute text-blue-600 flex items-center justify-center">
+      hello
+    </div>
+  );
+}
